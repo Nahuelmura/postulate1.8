3ef3550fd23ea85542c71defa436db77a5474a5f
SergioBaulia96 <sergiobrk2008@gmail.com>
2024-08-20 16:19:20 -0300

    Creando rama development

--- obj/Debug/net8.0/Postulate.AssemblyInfo.cs
+++ obj/Debug/net8.0/Postulate.AssemblyInfo.cs
@@ -15,7 +15,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Postulate")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9786292d1a6b8110eb38df854f9b9f7379e29758")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+76e10e853a85f698d76fd5300849ec0bb0dcc1fa")]
 [assembly: System.Reflection.AssemblyProductAttribute("Postulate")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Postulate")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
